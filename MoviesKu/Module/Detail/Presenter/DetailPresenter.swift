04501da5d2c6444cd94c8dec9a39dd40558de0c4
reydaniel <reynaldidaniel@outlook.com>
2021-11-30 22:55:41 +0700

    Edit HomeRouter

---
+++ MoviesKu/Module/Detail/Presenter/DetailPresenter.swift
@@ -0,0 +1,21 @@
+//
+//  DetailPresenter.swift
+//  MoviesKu
+//
+//  Created by ReyDaniel on 30/11/21.
+//
+
+import Foundation
+import Combine
+
+class DetailPresenter {
+    private let detailUseCase: DetailUseCase
+    
+    init(detailUseCase: DetailUseCase) {
+        self.detailUseCase = detailUseCase
+    }
+    
+    func getDetail(id: Int) -> AnyPublisher<DetailModel, Error> {
+        return detailUseCase.getDetail(id: id)
+    }
+}
